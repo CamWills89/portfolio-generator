574b05c6c676ae5f856c139a1a369b5e81e0d63a
AlphaC <cameron.wills89@outlook.com>
2020-09-21 16:30:10 -0400

    created new html page from filesystem module and modularized the code

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    for(let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+// const printProfileData = profileDataArr => {
+//     for(let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
 
-    console.log('================');
+//     console.log('================');
 
-    // profileDataArr.forEach(function(profileItem) {
-    //     console.log(profileItem);
-    // }); the arrow function does the same thing, but is cleaner
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//     // profileDataArr.forEach(function(profileItem) {
+//     //     console.log(profileItem);
+//     // }); the arrow function does the same thing, but is cleaner
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs)
+// printProfileData(profileDataArgs);
+
+//Initialize the file system module
+const fs = require('fs');
+
+//Importing the generatePage function from page-tenplate.js
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
+
+// console.log(name, github);
+// console.log(generatePage(name, github));
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
