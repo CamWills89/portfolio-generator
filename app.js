8ab7ffd69f4a12d8f0d10a88df2ae90f81125333
AlphaC <cameron.wills89@outlook.com>
2020-09-22 13:55:45 -0400

    added inquirer to project

--- app.js
+++ app.js
@@ -15,22 +15,33 @@
 
 // printProfileData(profileDataArgs);
 
-//Initialize the file system module
-const fs = require('fs');
+// Initialize the file system module
+// const fs = require('fs');
+const inquirer = require('inquirer');
 
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your Name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 //Importing the generatePage function from page-tenplate.js
-const generatePage = require('./src/page-template.js');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const profileDataArgs = process.argv.slice(2);
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 
 // console.log(name, github);
 // console.log(generatePage(name, github));
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
